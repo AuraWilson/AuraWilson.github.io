2d4391e8dfd39aac3f6b544a869774af9099ddc3
AuraWilson <wilsonaura2006@gmail.com>
2024-08-13 00:47:37 +0000

    hello

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let button = document.querySelector("#darkmode") ;
 let silly = document.querySelector("#lightmode") ;
 let crazy = document.querySelector("#nextpage") ;
+let back = document.querySelector("#lastpage") ;
 // select the element with id of darkmode
 
 // Function to handle button click
@@ -14,15 +15,19 @@ function handleClick() {
 
   function handleClicklightmode(){
     alert('Button was clicked!')
-    document.querySelector("body").style.cssText = "color: black; background-color: white;"
+    document.querySelector("body").style.cssText = "color: black; background-color: rgb(197, 222, 205);"
   }
 
   function handleClicknextpage(){
     alert('Onto the next page')
+  }
+  
+  function handleClicklastpage(){
+    alert('Returning to home')
   }
  
   // Add the click event listener
   button.addEventListener('click', handleClick);
   silly.addEventListener('click', handleClicklightmode);
   crazy.addEventListener('click', handleClicknextpage);
-  
+  crazy.addEventListener('click', handleClicklastpage);
